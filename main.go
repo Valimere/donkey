be2bd7e1a256730772e37aacc0ce94a6d017e2c9
Valimere <Valimere@gmail.com>
2024-04-04 11:23:37 -0700

    partial working

--- main.go
+++ main.go
@@ -6,13 +6,25 @@ import (
 	"fmt"
 	"golang.org/x/oauth2"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	//REDDIT_USER_AGENT = os.Getenv("REDDIT_USER_AGENT")
+	REDDIT_CLIENT_ID = os.Getenv("REDDIT_CLIENT_ID")
+	REDDIT_SECRET    = os.Getenv("REDDIT_SECRET")
+	//REDDIT_USERNAME   = os.Getenv("REDDIT_USERNAME")
+	//REDDIT_PASSWORD   = os.Getenv("REDDIT_PASSWORD")
+	DELAY = time.Duration(1)
 )
 
 var redditOAuthConfig = &oauth2.Config{
-	ClientID:     "YOUR_CLIENT_ID",
-	ClientSecret: "YOUR_CLIENT_SECRET",
-	RedirectURL:  "http://localhost:8080/callback", // Make sure this matches the redirect URI in your Reddit app settings
+	ClientID:     REDDIT_CLIENT_ID,
+	ClientSecret: REDDIT_SECRET,
+	RedirectURL:  "http://localhost:8080", // Make sure this matches the redirect URI in your Reddit app settings
 	Scopes:       []string{"read"},
 	Endpoint: oauth2.Endpoint{
 		AuthURL:  "https://www.reddit.com/api/v1/authorize",
@@ -25,7 +37,7 @@ func getRedditClient(ctx context.Context, token *oauth2.Token) *http.Client {
 }
 
 func fetchSubredditPosts(ctx context.Context, client *http.Client, subreddit string) ([]byte, error) {
-	resp, err := client.Get(fmt.Sprintf("https://oauth.reddit.com/r/%s/hot", subreddit))
+	resp, err := client.Get(fmt.Sprintf("https://oauth.reddit.com/r/%s/new", subreddit))
 	if err != nil {
 		return nil, err
 	}
@@ -41,21 +53,22 @@ func main() {
 	fmt.Printf("Go to the following link in your browser:\n%s\n", authURL)
 	fmt.Print("Enter the authorization code: ")
 	var authCode string
-	fmt.Scan(&authCode)
+	_, err := fmt.Scan(&authCode)
+	if err != nil {
+		log.Fatalf("error reading authcode %s", err)
+	}
+	log.Printf("authcode: %s", authCode)
 
+	time.Sleep(DELAY * time.Second)
 	token, err := redditOAuthConfig.Exchange(ctx, authCode)
 	if err != nil {
 		panic(err)
 	}
-
-	// Exchange the authorization code for a token
-	// This code should be obtained from the redirect URL query params after user authentication
-	//token, err := redditOAuthConfig.Exchange(ctx, "YOUR_AUTHORIZATION_CODE")
-	//if err != nil {
-	//	panic(err)
-	//}
-
+	log.Printf("token: %s", token)
 	client := getRedditClient(ctx, token)
+	log.Printf("client: %s", client)
+
+	time.Sleep(DELAY * time.Second)
 
 	subreddit := "news" // Specify the subreddit you want to fetch
 	posts, err := fetchSubredditPosts(ctx, client, subreddit)
