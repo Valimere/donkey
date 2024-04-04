e4b7d07b8def9cfc7f0f7c4964aa0632dea7456e
Valimere <Valimere@gmail.com>
2024-04-04 10:27:12 -0700

    saving work

--- main.go
+++ main.go
@@ -1,46 +1,70 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-	"strings"
+	"context"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"net/http"
 )
 
+var redditOAuthConfig = &oauth2.Config{
+	ClientID:     "YOUR_CLIENT_ID",
+	ClientSecret: "YOUR_CLIENT_SECRET",
+	RedirectURL:  "http://localhost:8080/callback", // Make sure this matches the redirect URI in your Reddit app settings
+	Scopes:       []string{"read"},
+	Endpoint: oauth2.Endpoint{
+		AuthURL:  "https://www.reddit.com/api/v1/authorize",
+		TokenURL: "https://www.reddit.com/api/v1/access_token",
+	},
+}
+
+func getRedditClient(ctx context.Context, token *oauth2.Token) *http.Client {
+	return redditOAuthConfig.Client(ctx, token)
+}
+
+func fetchSubredditPosts(ctx context.Context, client *http.Client, subreddit string) ([]byte, error) {
+	resp, err := client.Get(fmt.Sprintf("https://oauth.reddit.com/r/%s/hot", subreddit))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
+
 func main() {
-	subredditsArg := flag.String("r", "music", "comma-separated list of subreddits i.e. \"funny, music\"")
-	logFile := flag.String("log", "", "path to log file (optional)")
-	flag.Parse()
+	ctx := context.Background()
+	// Redirect the user to the authorization URL
+	authURL := redditOAuthConfig.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser:\n%s\n", authURL)
+	fmt.Print("Enter the authorization code: ")
+	var authCode string
+	fmt.Scan(&authCode)
 
-	// Configure logging
-	if *logFile != "" {
-		// creating a file if it doesn't exist, for write only, and appends, perms: readable and writable by all users
-		file, err := os.OpenFile(*logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("Error opening log file: %v", err)
-		}
-		defer func(file *os.File) {
-			err := file.Close()
-			if err != nil {
-				log.Fatalf("error trying to close the file %v", err)
-			}
-		}(file)
-		log.SetOutput(file)
-	} else {
-		log.SetOutput(os.Stdout)
+	token, err := redditOAuthConfig.Exchange(ctx, authCode)
+	if err != nil {
+		panic(err)
 	}
 
-	// Split the subredditsArg into a slice of uncleanSubreddits
-	uncleanSubreddits := strings.Split(*subredditsArg, ",")
+	// Exchange the authorization code for a token
+	// This code should be obtained from the redirect URL query params after user authentication
+	//token, err := redditOAuthConfig.Exchange(ctx, "YOUR_AUTHORIZATION_CODE")
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	// Create a new slice for the cleaned subreddits
-	var subreddits []string
-	for _, subreddit := range uncleanSubreddits {
-		subreddit = strings.TrimSpace(subreddit)
-		if subreddit != "" {
-			subreddits = append(subreddits, subreddit)
-		}
+	client := getRedditClient(ctx, token)
+
+	subreddit := "news" // Specify the subreddit you want to fetch
+	posts, err := fetchSubredditPosts(ctx, client, subreddit)
+	if err != nil {
+		panic(err)
 	}
 
-	log.Printf("\nStarting app, Subreddits chosen: %v\n", subreddits)
+	// For demonstration, printing JSON response
+	var jsonData map[string]interface{}
+	json.Unmarshal(posts, &jsonData)
+	fmt.Println(jsonData)
 }
